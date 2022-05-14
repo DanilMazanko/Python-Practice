2448016f295ffcc9ea534dc531380d44aee368f7
DanilMazanko <97046125+DanilMazanko@users.noreply.github.com>
2022-05-14 20:49:04 +0300

    Update bot.py
    Imported types from telebot. Added 2 inline buttons

--- Telegram Bot/bot.py
+++ Telegram Bot/bot.py
@@ -1,23 +1,33 @@
+from email import message
 from multiprocessing import parent_process
 import telebot
-
+from telebot import types
 bot = telebot.TeleBot('5361727946:AAHSoOOBoi3UrSaUJsx-KRpsmkYY95Lw5LE')
 
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hello fdf", parse_mode="html")
+def start2(message):
+    markup = types.InlineKeyboardMarkup()
+    item1 = types.InlineKeyboardButton(
+        "Граматика база", callback_data='grammar')
+    item2 = types.InlineKeyboardButton("IELTS",  callback_data='ielts')
+    markup.add(item1, item2)
+    bot.send_message(
+        message.chat.id, "Здравствуйте, над чем бы вы хотели рабоатать сегодня?", reply_markup=markup)
 
 
-@bot.message_handler()
-def get_user_text(message):
-    if message.text == "времена":
-        bot.send_message(message.chat.id, 'tensences', parse_mode='html')
-
-# @bot.message_handler(commands=['stop'])
-# def start(message):
-#     bot.send_message(message.chat.id, "Hello fdf", parse_mode="html")
-#     bot.polling(none_stop=False)
+@bot.callback_query_handler(func=lambda call: True)
+def callback_inline(call):
+    try:
+        if call.message:
+            if call.data == 'grammar':
+                bot.send_message(call.message.chat.id,
+                                 'Здесь будут ресурсы по граматики')
+            elif call.data == "ielts":
+                bot.send_message(call.message.chat.id,
+                                 'Здесь будут ресурсы')
+    except Exception as e:
+        print(repr(e))
 
 
 bot.polling(none_stop=True)
